f34f246fbb595c43ceb5b0a3192b162b6253059e
ShahidHashmi1 <hashmi.pgh@gmail.com>
2022-10-10 17:14:02 -0400

    restructure

--- server.js
+++ server.js
@@ -11,68 +11,69 @@ const db = require('./db/connection');
 
 db.connect((err) => {
   err ? console.error(err) : console.log("Successfully started server.");
-  starterPrompt();
+  init();
 });
 
+const init = () => {
+  const logoText = logo({ name: "Employee Manager" }).render();
+  console.log(logoText);
+  inquirer.prompt(starterPrompt)
+  .then((response) => {
+    const firstResponse = response.choices;
+    switch (firstResponse) {
+      case "View all employees":
+        viewAllEmployees();
+        break;
 
-const starterPrompt = () => {
-    const logoText = logo({ name: "Employee Manager" }).render();
-    console.log(logoText);
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        message: "What would you like to do?",
-        name: "choices",
-        choices: [
-          "View all employees",
-          "Add employee",
-          "Update employee role",
-          "View all roles",
-          "Add role",
-          "View all departments",
-          "Add department",
-          "Quit"
-        ],
-      },
-    ])
-    .then((response) => {
-      const firstResponse = response.choices;
-      switch (firstResponse) {
-        case "View all employees":
-          viewAllEmployees();
-          break;
+      case "View all roles":
+        viewAllRoles();
+        break;
 
-        case "View all roles":
-          viewAllRoles();
-          break;
+      case "View all departments":
+        viewAllDepartments();
+        break;
 
-        case "View all departments":
-          viewAllDepartments();
-          break;
+      case "Add employee":
+        addEmployee();
+        break;
 
-        case "Add employee":
-          addEmployee();
-          break;
+      case "Update employee role":
+        updateEmployeeRole();
+        break;
 
-        case "Update employee role":
-          updateEmployeeRole();
-          break;
+      case "Add role":
+        addRole();
+        break;
 
-        case "Add role":
-          addRole();
-          break;
+      case "Add department":
+        addDepartment();
+        break;
 
-        case "Add department":
-          addDepartment();
-          break;
-
-        case "Quit":
-          quit();
-          break;
-      }
-    });
+      case "Quit":
+        quit();
+        break;
+    }
+  });
 };
+
+
+const starterPrompt = [
+  {
+  type: "list",
+  message: "What would you like to do?",
+  name: "choices",
+  choices: [
+    "View all employees",
+    "Add employee",
+    "Update employee role",
+    "View all roles",
+    "Add role",
+    "View all departments",
+    "Add department",
+    "Quit"
+  ]
+}
+];
 
 const viewAllEmployees = async () => {
   db.query("SELECT * FROM employees;", (err, res) => {
@@ -80,71 +81,70 @@ const viewAllEmployees = async () => {
     const ve = console.table(res);
     console.log(ve);
   })
-  starterPrompt();
-};
+  init();
+}
 
-const viewAllRoles = (answers) => {
+const viewAllRoles = async () => {
   db.query("SELECT * FROM roles;", (err, res) => {
     if (err) throw err;
     const vr = console.table(res);
     console.log(vr);
   })
-  starterPrompt();
-};
+  init();
+}
 
-const viewAllDepartments = (answers) => {
+const viewAllDepartments = async () => {
   db.query("SELECT * FROM departments;", (err, res) => {
     if (err) throw err;
     const vd = console.table(res);
     console.log(vd);
-  });
-  starterPrompt();
-};
+  })
+  init();
+}
 
 const addEmployee = () => {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "Enter the employee's first name.",
-        name: "first_name",
-      },
-      {
-        type: "input",
-        message: "Enter the employee's last name.",
-        name: "last_name",
-      },
-      {
-        type: "input",
-        message: "Enter the employee's role ID number.",
-        name: "role_id",
-      },
-      {
-        type: "input",
-        message: "Enter the manager ID number.",
-        name: "manager_id",
-      },
-    ])
-    .then((answers) => {
-      db.query(`INSERT INTO employees VALUES ('${answers.first_name}', '${answers.last_name}', ${answers.role_id}, ${answers.manager_id});`, (err, res) => {
+  const questions = [
+    {
+      type: "input",
+      message: "Enter the employee's first name.",
+      name: "first_name",
+    },
+    {
+      type: "input",
+      message: "Enter the employee's last name.",
+      name: "last_name",
+    },
+    {
+      type: "input",
+      message: "Enter the employee's role ID number.",
+      name: "role_id",
+    },
+    {
+      type: "input",
+      message: "Enter the manager ID number.",
+      name: "manager_id",
+    }
+  ];
+
+    inquirer.prompt(questions).then((answers) => {
+      db.query(`INSERT INTO employees('first_name', 'last_name', role_id, manager_id) VALUES ('${answers.first_name}', '${answers.last_name}', ${answers.role_id}, ${answers.manager_id});`, (err, res) => {
         (err) ? console.error(err) : console.log('Employee successfully added.') ;
         console.table(res);
       });
-    });
-    // starterPrompt();
-};
+    })
+    init();
+}
 
-const addDepartment = async () => {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: 'What is the name of the department you would like to create?',
-        name: 'department_name'
-      }
-    ])
-    .then(answer);
-    db.query(`INSERT INTO departments VALUES('${answer.department_name}');`, (err, res) => {
+const addDepartment = () => {
+  const deptQ = [
+    {
+      type: "input",
+      message: 'What is the name of the department you would like to create?',
+      name: 'department_name'
+    }
+  ];
+    inquirer.prompt(deptQ).then(answer);
+    db.query(`INSERT INTO departments('department_name') VALUES('${answer.department_name}');`, (err, res) => {
       (err) ? console.error(err) : console.log('Department added successfully.');
       console.table(res);
     });
@@ -170,11 +170,11 @@ const addRole = async () => {
         name: "department_id"
       }
     ]).then(response);
-    db.query(`INSERT INTO roles VALUES ('${response.role_title}', ${response.salary}, ${response.department_id});`, (err, res) => {
+    db.query(`INSERT INTO roles ('title', 'salary', department_id) VALUES ('${response.role_title}', ${response.salary}, ${response.department_id});`, (err, res) => {
       (err) ? console.error(err) : console.log('Role added successfully.');
       console.table(res);
     })
-    await starterPrompt();
+    await inquirer.prompt(starterPrompt);
 }
 
 const updateEmployeeRole = async () => {
@@ -192,7 +192,7 @@ const updateEmployeeRole = async () => {
   // ]);
   // await db.query("UPDATE employees WHERE role_id IS _;");
   // figure out how to update employee role with mysql command
-  starterPrompt();
+
 };
 
 
